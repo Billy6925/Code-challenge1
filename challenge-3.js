980d4bdb3e5db380c265fdc2913b4fcff75c39c0
Billy Eskein <billyeskein@gmail.com>
2024-12-02 11:42:15 +0300

    Update challenge-3.js

--- challenge-3.js
+++ challenge-3.js
@@ -6,5 +6,22 @@ function calculateNetSalary (basicSalary, benefits) {
          { threshold: 467333, rate: 0.3 }, 
         { threshold: Infinity, rate: 0.35 } 
     ];
+    const NHIF_RATES = [
+         { maxSalary: 5999, rate: 150 }, 
+        { maxSalary: 7999, rate: 300 }, 
+        { maxSalary: 11999, rate: 400 },
+         { maxSalary: 14999, rate: 500 }, 
+         { maxSalary: 19999, rate: 600 }, 
+        { maxSalary: 24999, rate: 750 },
+         { maxSalary: 29999, rate: 850 }, 
+         { maxSalary: 34999, rate: 900 },
+          { maxSalary: 39999, rate: 950 },
+           { maxSalary: 44999, rate: 1000 },
+            { maxSalary: 49999, rate: 1100 }, 
+            { maxSalary: 59999, rate: 1200 }, 
+            { maxSalary: 69999, rate: 1300 },
+             { maxSalary: 79999, rate: 1400 }, 
+        { maxSalary: Infinity, rate: 1700 }
+     ];
    
 }
