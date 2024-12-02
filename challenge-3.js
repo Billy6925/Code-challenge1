efffee003f8537cf910ca81fec66697cc06d25ac
Billy Eskein <billyeskein@gmail.com>
2024-12-02 09:20:33 +0300

    Update challenge-3.js

--- challenge-3.js
+++ challenge-3.js
@@ -6,7 +6,19 @@ function calculateNetSalary (basicSalary, benefits) {
  let nssfDeduction;
  if (grossSalary <= 18000) {
     nssfDeduction = grossSalary * 0.06; //6% 0f gross for Tier I
+ }else {
+    nssfDeduction =1080; // Fixed maximum contribution
  }
+ // Calculate NHIF Deductions
+ const nhifRates = [
+    {min: 0, max: 5999, deduction: 150},
+    {min:6000, max: 7999, deduction: 300},
+    {min:8000, max: 11999, deduction: 400},
+    {min:12000, max:14999, deduction: 500},
+    {min: 15000, max: 19999, deduction: 600},
+    {min:20000, max:24999, deduction: 750},
+    
+ ]
 
         
 }
