db4fb867d59fd1094826f1b276da59c9455eed7a
Billy Eskein <billyeskein@gmail.com>
2024-12-02 06:43:22 +0300

    Update challenge-1.js

--- challenge-1.js
+++ challenge-1.js
@@ -2,4 +2,7 @@ function inputSudentGrade () {
     //prompts the user to enter student's marks
     const marks = parseInt(prompt("Please enter student marks (between 0 and 100): "), 10);
     // Ensure that the input is between 0 and 100
+    if (marks < 0 || marks > 100 || isNaN(marks)) {
+        console.log("Invalid marks entered. Please enter a number between 0 and 100."); 
+        return;
 }
