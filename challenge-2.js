04400ef1a1a5ba4052d10ea2bd891d271f1d45f0
Billy Eskein <billyeskein@gmail.com>
2024-12-02 08:02:10 +0300

    Update challenge-2.js

--- challenge-2.js
+++ challenge-2.js
@@ -2,12 +2,16 @@ function inputCarSpeed () {
     //prompts the user to enter car speed
     const speedLimit = 70;
     const kmPerDemeritPoint = 5;
+    //for every 5km above speedLimit, the driver gets one demerit point
     if (speed < speedLimit) {
         console.log('Ok')
     }else{
         const demeritPoints = Math.floor((speed - speedLimit) / kmPerDemeritPoint);
-         if (demeritPoints > 12) { console.log("License suspended");
+         if (demeritPoints > 12)
+             { console.log("License suspended");
+                // a driver with 12 demerit ponts gets his licence suspended
     }else{
-        
+        console.log(`Points: ${demeritPoints}`);
     }
+}
 }
