7fa192be6d8d3ccd2c9e91097e66fa7789d6d4c5
Billy Eskein <billyeskein@gmail.com>
2024-12-02 07:52:01 +0300

    Update challenge-2.js

--- challenge-2.js
+++ challenge-2.js
@@ -1,4 +1,13 @@
 function inputCarSpeed () {
     //prompts the user to enter car speed
-    
+    const speedLimit = 70;
+    const kmPerDemeritPoint = 5;
+    if (speed < speedLimit) {
+        console.log('Ok')
+    }else{
+        const demeritPoints = Math.floor((speed - speedLimit) / kmPerDemeritPoint);
+         if (demeritPoints > 12) { console.log("License suspended");
+    }else{
+        
+    }
 }
