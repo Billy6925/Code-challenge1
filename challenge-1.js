93ce092b29385d9a297d854c13b60d77a2cda53a
Billy Eskein <billyeskein@gmail.com>
2024-12-02 07:01:43 +0300

    Update challenge-1.js

--- challenge-1.js
+++ challenge-1.js
@@ -6,3 +6,16 @@ function inputSudentGrade () {
         console.log("Invalid marks entered. Please enter a number between 0 and 100."); 
         return;
 }
+//Determine the grade based on marks
+let grade;
+if (marks >79) {
+    grade = 'A';
+}else if (marks >= 60 && marks <= 79) {
+    grade = 'B'
+}else if (marks >=49 && marks <= 59) {
+    grade = 'C'
+}else if (marks >= 40 && marks <= 49) {
+    grade = 'D'
+}else if (marks < 40) {
+    grade = 'E'
+}
