241380e16563b062de23044a96e1249dc90da573
Billy Eskein <billyeskein@gmail.com>
2024-12-05 07:46:25 +0300

    Update challenge-1.js

--- challenge-1.js
+++ challenge-1.js
@@ -1,26 +1,19 @@
-function inputSudentGrade () {
-    //prompts the user to enter student's marks
-    const marks = parseInt(prompt("Please enter student marks (between 0 and 100): "), 10);
-    // Ensure that the input is between 0 and 100
-    if (marks < 0 || marks > 100 || isNaN(marks)) {
-        console.log("Invalid marks entered. Please enter a number between 0 and 100."); 
-        return;
-}
-//Determine the grade based on marks
-let grade;
-if (marks >79) {
-    grade = 'A';
-}else if (marks >= 60 && marks <= 79) {
-    grade = 'B'
-}else if (marks >=49 && marks <= 59) {
-    grade = 'C'
-}else if (marks >= 40 && marks <= 49) {
-    grade = 'D'
-}else if (marks < 40) {
-    grade = 'E'
-}
-// Output the grade
-console.log(`The grade for marks ${marks} is: ${grade}`
-}
-// Call the function to prompt the user and display the grade
-inputSudentGrade ( );
+function inputStudentMarks(marks) {
+
+    let grade;
+    if(marks > 79 && marks <= 100) {
+        return (grade = 'A');
+    }else if(marks >= 60 && marks <= 79) {
+        return (grade = 'B');
+    }else if(marks > 49 && marks <= 59) {
+        return (grade = 'C');
+    }else if(marks >=40 && marks < 49) {
+        return (grade = 'D');
+    }else if(marks >= 0 && marks < 40) {
+        return (grade = 'E');
+    }else{
+return 'invalid'
+    }
+    }
+
+inputStudentMarks();
